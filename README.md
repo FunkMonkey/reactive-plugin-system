2075e9b5b8c0e0f664fda7c7855aaeef4ca77d66
AndSelm <and.selm+git@gmail.com>
2018-02-16 18:04:58 +0100

    DOCS: Readme examples and jsdoc

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # reactive-plugin-system
+
+`reactive-plugin-system` is an addon / extension / plugin system for JavaScript.
+It uses [RxJS5](https://github.com/ReactiveX/rxjs) to handle asynchronous loading
+and unloading of plugins. Plugins can depend on each other.
+
+## Example
+
+A simple plugin system that uses `require` to load plugins.
+
+### some-app.js
+
+```js
+import { PluginSystem, factoryLoaders } from 'reactive-plugin-system';
+
+const plugins = new PluginSystem( {
+  data: 'shared',
+  getFactory: factoryLoaders.require
+} );
+
+plugins.loadAndWaitForAll( 'plugin-a', `plugin-b`, `plugin-c` );
+  .subscribe( ( [pluginA, pluginB, pluginC] ) => {
+    pluginB.doSomething();
+    plugins.unload( 'pluginC' ); // unloading and cleaning up
+  } );
+```
+
+### plugin-a/index.js
+
+`PluginA` loads asynchronously and uses shared data.
+
+```js
+class PluginA {
+  constructor( data ) {
+    this.data = data;
+  }
+
+  printData() {
+    console.log( this.data );
+  }
+}
+
+export default function pluginAFactory( data ) {
+  return Rx.Observable.timer( 1000 ) // wait a second
+    .map( () => new PluginA( data ) );
+}
+```
+
+### plugin-b/index.js
+
+`PluginB` depends on `PluginA`.
+
+```js
+class PluginB {
+  constructor( pluginA ) {
+    this.pluginA = pluginA;
+  }
+
+  doSomething() {
+    this.pluginA.printData();
+  }
+}
+
+export default function pluginBFactory( data, pluginSystem ) {
+  return pluginSystem.waitFor( 'plugin-a' )
+    .map( pluginA => new PluginB( pluginA ) );
+}
+```
+
+### plugin-c/index.js
+
+`PluginC` needs to clean up in the end.
+
+```js
+import { Observable } from 'rxjs';
+
+class PluginC {
+  cleanup() {
+    // do cleanup
+  }
+}
+
+export default function pluginCFactory() {
+  return new Observable( observer => {
+    const pluginC = new PluginC();
+
+    observer.next( pluginC );
+    observer.complete();
+
+    return () => pluginC.cleanup();
+  } );
+}
+```
+
+## API
+
+### PluginSystem
+
+#### Constructor
--- src/index.js
+++ src/index.js
@@ -35,7 +35,43 @@ function getID( idOrPluginInfo ) {
   return idOrPluginInfo.id;
 }
 
+/**
+ *
+ * @typedef {Object} PluginInfo
+ * @property {string}         id           Unique id of the plugin
+ * @property {pluginFactory}  [factory]    The associated plugin factory
+ * @property {}               [exports]    Exports of the plugin
+ */
+
+/**
+ * Factory function for plugins
+ * @callback pluginFactory
+ * @param   {}               data           Shared data given to all factory functions
+ * @param   {PluginSystem}   pluginSystem   The plugin system that called the factory
+ * @return  {Observable.<any>}              Exports of the factory - can be anything
+ */
+
+
+/**
+ * A function that returns a factory function
+ *
+ * @callback getFactoryCB
+ * @param   {PluginInfo}      pluginInfo   Plugin information that contains at least an `id`
+ * @return  {Observable.<pluginFactory>}   The factory function
+ */
+
+
+/**
+ * Represents the plugin system
+ */
 export class PluginSystem {
+  /**
+   * Creates a `PluginSystem` instance.
+   *
+   * @param    {}               data         Shared data that is passed to plugin factory functions
+   * @param    {getFactoryCB}   getFactory   Callback function that is used to get a plugin factory.
+   *                                         Useful to provide a way of loading the plugins.
+   */
   constructor( { data, getFactory } ) {
     this.data = data;
     this.getFactory = getFactory;
@@ -44,8 +80,22 @@ export class PluginSystem {
     if ( !getFactory )
       getFactory = factoryLoaders.standard;
 
+    /**
+     * Subject that takes `PluginInfo`s or ids to load plugins
+     * @type   {Rx.ReplaySubject.<PluginInfo|string>}
+     */
     this.toLoad$ = new Rx.ReplaySubject();
+
+    /**
+     * Subject that takes `PluginInfo`s or ids to unload plugins
+     * @type   {Rx.ReplaySubject.<PluginInfo|string>}
+     */
     this.toUnload$ = new Rx.ReplaySubject();
+
+    /**
+     * Observable of loaded `PluginInfo`s
+     * @type   {Rx.Observable.<PluginInfo>}
+     */
     this.loaded$ = this.toLoad$
       .map( stringToPluginInfo )
       // TODO: handle already loaded plugins
@@ -54,9 +104,15 @@ export class PluginSystem {
       .publishReplay()
       .refCount(); // TODO: use `shareReplay`
 
+    /**
+     * Observable of unloaded `PluginInfo`s
+     * @type   {Rx.Observable.<PluginInfo>}
+     */
     this.unloaded$ = this.toUnload$
       .map( stringToPluginInfo )
-      .do( pluginInfo => { this.loaded[pluginInfo.id].subscription.unsubscribe(); } );
+      .do( pluginInfo => { this.loaded[pluginInfo.id].subscription.unsubscribe(); } )
+      .publishReplay()
+      .refCount(); // TODO: use `shareReplay`
 
     // TODO: don't forward error to console here
     this.loadSubscription = this.loaded$
@@ -65,7 +121,13 @@ export class PluginSystem {
       .subscribe( () => {}, err => console.error( err, err.stack ) )
   }
 
+  /**
+   * Destroys the plugin-system by unsubscribing.
+   * If no one else subscribed to `loaded$` or `unloaded$` this will result in
+   * plugins cleaning up.
+   */
   destroy() {
+    // TODO: force unload of all loaded plugins?
     this.loadSubscription.unsubscribe();
     this.unloadSubscription.unsubscribe();
   }
@@ -95,6 +157,11 @@ export class PluginSystem {
     } );
   }
 
+  /**
+   * Waits for a plugin and returns its exports as an Observable
+   * @param    {PluginInfo|string}   idOrPluginInfo   Plugin to wait for
+   * @return   {Rx.Observable.<any>}                  Exports of the plugin
+   */
   waitFor( idOrPluginInfo ) {
     return this.loaded$
       .filter( ( { id } ) => id === getID( idOrPluginInfo ) )
@@ -102,6 +169,11 @@ export class PluginSystem {
       .map( ( { exports } ) => exports );
   }
 
+  /**
+   * Waits for a plugin and returns its exports as an Observable
+   * @param    {PluginInfo|string}   idOrPluginInfo   Plugin to wait for
+   * @return   {Rx.Observable.<any>}                  Exports of the plugin
+   */
   waitForAll( pluginIDs ) {
     return Rx.Observable.combineLatest( pluginIDs.map( this.waitFor.bind( this ) ) );
   }
